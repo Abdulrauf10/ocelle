07f377df9de4230e01ee1655b70c4f34073c2fd4
Ryan Har <ryan.har@amus.dev>
2024-01-11 07:19:10 +0800

    Update calculating page

--- src/app/get-started/fragments/Calculating.tsx
+++ src/app/get-started/fragments/Calculating.tsx
@@ -1,3 +1,21 @@
+import Container from '@/components/Container';
+import H2 from '@/components/Heading/H2';
+import Image from 'next/image';
+
 export default function CalculatingFragment() {
-  return <div></div>;
+  return (
+    <Container className="text-center">
+      <Image
+        src="/question/loading.gif"
+        alt="loading indicator"
+        width={200}
+        height={200}
+        className="inline-block"
+      />
+      <H2 className="mt-[30px] font-bold text-primary">Calculating...</H2>
+      <p className="mt-[30px] text-primary">
+        We’re crunching some numbers to formulate [Charlie]’s meal plan.
+      </p>
+    </Container>
+  );
 }
