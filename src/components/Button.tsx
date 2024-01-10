9490d5dede180438b06aa1e8e31585998c997bcf
Ryan Har <ryan.har@amus.dev>
2024-01-11 03:10:46 +0800

    Add link ability to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,34 @@
 import clsx from 'clsx';
+import Link from 'next/link';
 import React from 'react';
 
 interface ButtonProps {
   className?: string;
 }
 
-export default function Button({ children, className }: React.PropsWithChildren<ButtonProps>) {
-  return (
-    <button
-      className={clsx(
-        'inline-block cursor-pointer rounded-[30px] bg-secondary bg-dogfoot-icon bg-[length:25px_auto] bg-[center_right_15px] bg-no-repeat py-[10px] pl-[25px] pr-[50px] text-center text-[18px] font-bold text-white transition-all duration-300 ease-in-out hover:bg-[#EA6A00]',
-        className
-      )}
-    >
-      {children}
-    </button>
+interface LinkbuttonProps extends ButtonProps {
+  href: string;
+  target?: React.HTMLAttributeAnchorTarget;
+}
+
+export default function Button({
+  children,
+  className,
+  ...props
+}: React.PropsWithChildren<ButtonProps | LinkbuttonProps>) {
+  const linkProps = props as LinkbuttonProps;
+  const classes = clsx(
+    'inline-block cursor-pointer rounded-[30px] bg-secondary bg-dogfoot-icon bg-[length:25px_auto] bg-[center_right_15px] bg-no-repeat py-[10px] pl-[25px] pr-[50px] text-center text-[18px] font-bold text-white transition-all duration-300 ease-in-out hover:bg-[#EA6A00]',
+    className
   );
+
+  if (linkProps.href) {
+    return (
+      <Link href={linkProps.href} className={classes} target={linkProps.target}>
+        {children}
+      </Link>
+    );
+  }
+
+  return <button className={classes}>{children}</button>;
 }
