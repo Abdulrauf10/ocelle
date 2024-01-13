ac42f11241cb275b41e6ec2158b8ce66e3c2c05e
Ryan Har <ryan.har@amus.dev>
2024-01-13 14:15:14 +0800

    Align newsletter block

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -2,26 +2,30 @@ import Container from './Container';
 
 export default function Newsletter() {
   return (
-    <div className="bg-[#dce7ef] px-4 py-6">
+    <div className="bg-[#dce7ef] py-8">
       <Container>
-        <div className="-mx-3 flex flex-nowrap items-center justify-center text-xl max-md:block">
-          <div className="m-3 flex min-h-[38px] items-center bg-newsletter-icon bg-[length:60px_auto] bg-[left_center] bg-no-repeat pl-20">
-            <span>
-              Exclusive insights, special offers, and helpful nutrition information from the Ocelle
-              Dog Pack.{' '}
-              <strong className="text-primary">
-                Join Now! <i>Woof!</i>
-              </strong>
-            </span>
+        <div className="-m-3 flex flex-nowrap items-center justify-center text-xl max-md:block">
+          <div className="w-full p-3">
+            <div className="flex min-h-[38px] items-center bg-newsletter-icon bg-[length:60px_auto] bg-[left_center] bg-no-repeat pl-20">
+              <span>
+                Exclusive insights, special offers, and helpful nutrition information from the
+                Ocelle Dog Pack.{' '}
+                <strong className="text-primary">
+                  Join Now! <i>Woof!</i>
+                </strong>
+              </span>
+            </div>
           </div>
-          <form className="relative flex w-[340px] min-w-[340px] items-center max-md:w-full">
-            <input
-              type="email"
-              className="m-3 w-full rounded-[30px] border-0 bg-white py-4 pl-5 pr-[55px]"
-              placeholder="Enter your email address"
-            />
-            <button className="absolute right-[20px] h-[44px] w-[44px] cursor-pointer border-0 bg-newsletter-btn bg-[length:100%_auto] bg-center bg-no-repeat"></button>
-          </form>
+          <div className="p-3">
+            <form className="relative flex w-[340px] min-w-[340px] items-center max-md:w-full">
+              <input
+                type="email"
+                className="w-full rounded-[30px] border-0 bg-white py-4 pl-6 pr-16"
+                placeholder="Enter your email address"
+              />
+              <button className="absolute right-2 h-11 w-11 cursor-pointer border-0 bg-newsletter-btn bg-[length:100%_auto] bg-center bg-no-repeat"></button>
+            </form>
+          </div>
         </div>
       </Container>
     </div>
