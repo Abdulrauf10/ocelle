995e6620ff02e93fee0779085024dfb8a654c985
Ryan Har <ryan.har@amus.dev>
2024-02-09 14:01:56 +0800

    Update food allergies enum

--- src/enums/FoodAllergies.ts
+++ src/enums/FoodAllergies.ts
@@ -1,10 +1,10 @@
 enum FoodAllergies {
-  None = 0,
-  Chicken = 1 << 0,
-  Beef = 1 << 1,
-  Pork = 1 << 2,
-  Lamb = 1 << 3,
-  Duck = 1 << 4,
+  None = 1 << 0,
+  Chicken = 1 << 1,
+  Beef = 1 << 2,
+  Pork = 1 << 3,
+  Lamb = 1 << 4,
+  Duck = 1 << 5,
 }
 
 export default FoodAllergies;
