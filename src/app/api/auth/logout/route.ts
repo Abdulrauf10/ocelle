bb80f46c8baf4c7c76ba31b53313e3bb3169ba18
Ryan Har <ryan.har@amus.dev>
2024-02-11 01:56:23 +0800

    Add forgot password api route

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,5 +1,4 @@
 import { NextResponse } from 'next/server';
-import Joi from 'joi';
 import saleorAuthClient from '@/saleorAuthClient';
 
 export const dynamic = 'force-dynamic'; // defaults to auto
