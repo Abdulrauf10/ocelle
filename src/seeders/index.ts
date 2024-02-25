ab99deb0fe149b682eeb195663e77c272f6248d3
Ryan Har <ryan.har@amus.dev>
2024-02-25 17:47:42 +0800

    Add dog seeder

--- src/seeders/index.ts
+++ src/seeders/index.ts
@@ -1,5 +1,6 @@
 import Seeder from './Seeder';
 import BreedSeeder from './BreedSeeder';
 import CareerSeeder from './CareerSeeder';
+import DogSeeder from './DogSeeder';
 
-export default [new BreedSeeder(), new CareerSeeder()] as Seeder[];
+export default [new BreedSeeder(), new CareerSeeder(), new DogSeeder()] as Seeder[];
