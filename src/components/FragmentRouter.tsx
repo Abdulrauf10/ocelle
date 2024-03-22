e4b57d9423b2e4c51a220cff8fa6b7fb198ff765
Ryan Har <ryan.har@amus.dev>
2024-03-23 06:58:06 +0800

    Fix fragment router state bugs

--- src/components/FragmentRouter.tsx
+++ src/components/FragmentRouter.tsx
@@ -2,8 +2,8 @@ import { AnimatePresence, motion } from 'framer-motion';
 import React from 'react';
 
 export interface FragmentProps<T> {
-  state: any;
-  navigate(name: T | -1, options?: { state: any; replace?: boolean }): void;
+  state?: Object;
+  navigate(name: T | -1, options?: { state?: Object; replace?: boolean }): void;
 }
 
 interface useFragmentRouterControllerProps<T> {
@@ -20,22 +20,22 @@ interface useFragmentRouterControllerReturn<T> {
     component: React.FunctionComponent<FragmentProps<T>>;
   }>;
   route?: T;
-  state?: any;
-  navigate(name: T | -1, options?: { state: any; replace?: boolean }): void;
+  state?: Object;
+  navigate(name: T | -1, options?: { state?: Object; replace?: boolean }): void;
 }
 
 export function useFragmentRouterController<T>({
   routes,
   defaultRoute,
 }: useFragmentRouterControllerProps<T>): useFragmentRouterControllerReturn<T> {
-  const stackRef = React.useRef<{ route: T; state: any }[]>([]);
-  const [currentState, setCurrentState] = React.useState<any>();
+  const stackRef = React.useRef<{ route: T; state?: Object }[]>([]);
+  const [currentState, setCurrentState] = React.useState<Object>();
   const [currentRoute, setCurrentRoute] = React.useState<T | undefined>(
     defaultRoute || routes[0]?.name
   );
 
   const navigate = React.useCallback(
-    (name: T | -1, options?: { state: any; replace?: boolean }) => {
+    (name: T | -1, options?: { state?: Object; replace?: boolean }) => {
       if (name === -1) {
         const item = stackRef.current.pop();
         if (item != null) {
