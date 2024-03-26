1094d3d35907d2e53e37ae1674a73c4098e9af61
Ryan Har <ryan.har@amus.dev>
2024-03-26 09:12:47 +0800

    Disable webpack typeorm warning message

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@ const createNextIntlPlugin = require('next-intl/plugin');
 const bundleAnalyzer = require('@next/bundle-analyzer');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack(config) {
+    config.ignoreWarnings = [
+      ...(config.ignoreWarnings || []),
+      { module: /typeorm/ },
+      { module: /app-root-path/, message: /the request of a dependency is an expression/ },
+    ];
+    return config;
+  }
+}
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
