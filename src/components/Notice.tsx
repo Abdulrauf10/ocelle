c5eb55836283d1426dfa7af6a060574cfc57e5f0
Ryan Har <ryan.har@amus.dev>
2024-03-26 14:06:20 +0800

    Disable notice show when get started complete page

--- src/components/Notice.tsx
+++ src/components/Notice.tsx
@@ -1,9 +1,16 @@
-import { Link } from '@/navigation';
+'use client';
+
+import { Link, usePathname } from '@/navigation';
 import Container from './Container';
 import { useTranslations } from 'next-intl';
 
 export default function Notice() {
   const t = useTranslations();
+  const pathname = usePathname();
+
+  if (pathname === '/get-started/complete') {
+    return undefined;
+  }
 
   return (
     <div className="bg-primary py-2 text-center text-xl text-white max-xl:text-base">
