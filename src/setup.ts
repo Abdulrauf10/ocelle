6f05e0dbe9f543efcb8da58c609cee7dc8a4f4c7
Ryan Har <ryan.har@amus.dev>
2024-03-28 08:15:01 +0800

    Fix setup to use object instead of id

--- src/setup.ts
+++ src/setup.ts
@@ -3,6 +3,7 @@ import AppDataSource from '@/AppDataSource';
 import { executeQuery } from './helpers/queryRunner';
 import { executeGraphQL } from './helpers/graphql';
 import {
+  CategoryFragment,
   ChannelFragment,
   CountryCode,
   CreateChannelDocument,
@@ -15,15 +16,18 @@ import {
   GetAllShippingZonesDocument,
   GetAllWarehousesDocument,
   GetChannelDocument,
+  ProductFragment,
+  ProductTypeFragment,
   ShippingZoneFragment,
   UpdateShippingMethodChannelListingDocument,
   UpdateShippingZoneDocument,
+  WarehouseFragment,
 } from './gql/graphql';
 import invariant from 'ts-invariant';
 
 async function findOrCreateChannel(
   shippingZones: ShippingZoneFragment[],
-  warehouses: string[]
+  warehouses: WarehouseFragment[]
 ): Promise<ChannelFragment> {
   invariant(process.env.SALEOR_CHANNEL_SLUG, 'Missing SALEOR_CHANNEL_SLUG env variable');
 
@@ -55,7 +59,7 @@ async function findOrCreateChannel(
         slug: process.env.SALEOR_CHANNEL_SLUG,
         currencyCode: 'HKD',
         defaultCountry: CountryCode.Hk,
-        addWarehouses: warehouses,
+        addWarehouses: warehouses.map((warehouse) => warehouse.id),
         addShippingZones: shippingZones.map((zone) => zone.id),
         orderSettings: {
           allowUnpaidOrders: false,
@@ -73,7 +77,7 @@ async function findOrCreateChannel(
   return channelCreate.channel!;
 }
 
-async function findOrCreateProductType() {
+async function findOrCreateProductType(): Promise<ProductTypeFragment> {
   console.log('execute find or create product type...');
 
   const { productTypes } = await executeGraphQL(FindProductTypesDocument, {
@@ -103,13 +107,13 @@ async function findOrCreateProductType() {
       throw new Error('failed to create product type');
     }
 
-    return productTypeCreate.productType?.id;
+    return productTypeCreate.productType!;
   } else {
-    return productTypes.edges[0].node.id;
+    return productTypes.edges[0].node;
   }
 }
 
-async function findOrCreateCategory() {
+async function findOrCreateCategory(): Promise<CategoryFragment> {
   console.log('execute find or create category...');
 
   const { category } = await executeGraphQL(FindProductCategoryDocument, {
@@ -122,31 +126,31 @@ async function findOrCreateCategory() {
     },
   });
 
-  if (!category) {
-    // create new product type for product
-    const { categoryCreate } = await executeGraphQL(CreateProductCategoryDocument, {
-      withAuth: false,
-      headers: {
-        Authorization: `Bearer ${process.env.SALEOR_APP_TOKEN}`,
-      },
-      variables: {
-        name: 'Ocelle',
-        slug: 'ocelle',
-      },
-    });
-
-    if (!categoryCreate || categoryCreate.errors.length > 0) {
-      categoryCreate && console.error(categoryCreate.errors);
-      throw new Error('failed to create product category');
-    }
-
-    return categoryCreate.category?.id;
-  } else {
-    return category.id;
+  if (category) {
+    return category;
   }
+
+  // create new product type for product
+  const { categoryCreate } = await executeGraphQL(CreateProductCategoryDocument, {
+    withAuth: false,
+    headers: {
+      Authorization: `Bearer ${process.env.SALEOR_APP_TOKEN}`,
+    },
+    variables: {
+      name: 'Ocelle',
+      slug: 'ocelle',
+    },
+  });
+
+  if (!categoryCreate || categoryCreate.errors.length > 0) {
+    categoryCreate && console.error(categoryCreate.errors);
+    throw new Error('failed to create product category');
+  }
+
+  return categoryCreate.category!;
 }
 
-async function findWarehouses() {
+async function findWarehouses(): Promise<WarehouseFragment[]> {
   console.log('execute find warehouses...');
 
   const { warehouses } = await executeGraphQL(GetAllWarehousesDocument, {
@@ -156,10 +160,14 @@ async function findWarehouses() {
     },
   });
 
-  return warehouses && warehouses.edges.map((warehouse) => warehouse.node.id);
+  if (!warehouses) {
+    throw new Error('failed to find warehouses');
+  }
+
+  return warehouses.edges.map((warehouse) => warehouse.node);
 }
 
-async function findShippingZones() {
+async function findShippingZones(): Promise<ShippingZoneFragment[] | null | undefined> {
   console.log('execute find shipping zones...');
 
   const { shippingZones } = await executeGraphQL(GetAllShippingZonesDocument, {
@@ -169,7 +177,7 @@ async function findShippingZones() {
     },
   });
 
-  return shippingZones && shippingZones.edges.map((shippingZone) => shippingZone.node);
+  return shippingZones && shippingZones.edges.map((shippingZone) => shippingZone.node!);
 }
 
 async function setupShippingZones(shippingZones: ShippingZoneFragment[]) {
@@ -195,12 +203,15 @@ async function setupShippingZones(shippingZones: ShippingZoneFragment[]) {
   }
 }
 
-async function setupShippingMethods(shippingZones: ShippingZoneFragment[], channelId: string) {
+async function setupShippingMethods(
+  shippingZones: ShippingZoneFragment[],
+  channel: ChannelFragment
+) {
   console.log('execute setup shipping methods...');
 
   for (const shippingZone of shippingZones) {
     for (const { id, channelListings } of shippingZone.shippingMethods ?? []) {
-      if (channelListings?.find(({ channel }) => channel.id === channelId)) {
+      if (channelListings?.find((listing) => listing.channel.id === channel.id)) {
         continue;
       }
       const { shippingMethodChannelListingUpdate } = await executeGraphQL(
@@ -215,7 +226,7 @@ async function setupShippingMethods(shippingZones: ShippingZoneFragment[], chann
             input: {
               addChannels: [
                 {
-                  channelId,
+                  channelId: channel.id,
                   price: 0,
                 },
               ],
@@ -236,11 +247,11 @@ async function setupShippingMethods(shippingZones: ShippingZoneFragment[], chann
 }
 
 async function setupSubscriptionProducts(
-  channel: string,
-  productType: string,
-  category: string,
-  warehouses: string[]
-) {
+  channel: ChannelFragment,
+  productType: ProductTypeFragment,
+  category: CategoryFragment,
+  warehouses: WarehouseFragment[]
+): Promise<ProductFragment> {
   invariant(process.env.SALEOR_PRODUCT_SLUG, 'Missing SALEOR_PRODUCT_SLUG env variable');
 
   console.log('setup products...');
@@ -265,7 +276,7 @@ async function setupSubscriptionProducts(
   });
 
   if (product) {
-    return product.id;
+    return product;
   }
 
   // create a product if not exists
@@ -290,11 +301,11 @@ async function setupSubscriptionProducts(
         version: '2.22.2',
       }),
       slug: process.env.SALEOR_PRODUCT_SLUG,
-      productType,
-      category,
+      productType: productType.id,
+      category: category.id,
       channelListings: [
         {
-          channelId: channel,
+          channelId: channel.id,
           isAvailableForPurchase: true,
           isPublished: true,
           publishedAt: new Date().toISOString(),
@@ -308,13 +319,13 @@ async function setupSubscriptionProducts(
           trackInventory: false,
           stocks: warehouses.map((warehouse) => {
             return {
-              warehouse,
+              warehouse: warehouse.id,
               quantity: Math.pow(2, 31) - 1,
             };
           }),
           channelListings: [
             {
-              channelId: channel,
+              channelId: channel.id,
               price: 1,
             },
           ],
@@ -346,19 +357,13 @@ async function setup() {
 
   await AppDataSource.destroy();
 
-  const productTypeId = await findOrCreateProductType();
-  if (!productTypeId) {
-    throw new Error('failed to create or find the product type');
-  }
+  const productType = await findOrCreateProductType();
 
-  const categoryId = await findOrCreateCategory();
-  if (!categoryId) {
-    throw new Error('failed to create or find the category');
-  }
+  const category = await findOrCreateCategory();
 
   const warehouses = await findWarehouses();
-  if (!warehouses) {
-    throw new Error('failed find the warehouses');
+  if (warehouses.length === 0) {
+    throw new Error('saleor contains 0 of warehouse');
   }
 
   const shippingZones = await findShippingZones();
@@ -369,18 +374,10 @@ async function setup() {
   await setupShippingZones(shippingZones);
 
   const channel = await findOrCreateChannel(shippingZones, warehouses);
-  if (!channel) {
-    throw new Error('failed to find the channel');
-  }
 
-  await setupShippingMethods(shippingZones, channel.id);
+  await setupShippingMethods(shippingZones, channel);
 
-  const product = await setupSubscriptionProducts(
-    channel.id,
-    productTypeId,
-    categoryId,
-    warehouses
-  );
+  const product = await setupSubscriptionProducts(channel, productType, category, warehouses);
   if (!product) {
     throw new Error('failed to create the subscription products');
   }
