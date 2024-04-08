c572567505806d4a92913dcc4b4b233c9b70554f
Ryan Har <ryan.har@amus.dev>
2024-04-09 03:59:50 +0800

    Remove console.log

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -26,8 +26,6 @@ export default async function RootLayout({
 }) {
   const me = await getClientLoginedMe();
 
-  console.log(typeof me);
-
   let messages;
   try {
     messages = (await import(`../../../messages/${locale}.json`)).default;
