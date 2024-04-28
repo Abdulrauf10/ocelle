09e7ba93ea36c8fe75ba7ee24739eb9a1e8c4693
Ryan Har <ryan.har@amus.dev>
2024-04-29 00:26:03 +0800

    Update delivery note

--- src/components/ShippableNote.tsx
+++ src/components/ShippableNote.tsx
@@ -1,8 +1,9 @@
+import { startOfDay } from 'date-fns';
+import { useTranslations } from 'next-intl';
+
 import type { Shipment } from '@/entities';
 import { formatDate } from '@/helpers/date';
 import { isDeliveredBox } from '@/helpers/dog';
-import { startOfDay } from 'date-fns';
-import { useTranslations } from 'next-intl';
 
 export function ShippableNote({ shipments }: { shipments: Shipment[] }) {
   const refDate = startOfDay(new Date());
@@ -14,24 +15,22 @@ export function ShippableNote({ shipments }: { shipments: Shipment[] }) {
     (shipment) => !isDeliveredBox(shipment.deliveryDate) && shipment.editableDeadline <= refDate
   );
 
-  if (!shipable) {
-    throw new Error('should have shipment available to upcoming boxs');
-  }
-
   // TODO: should hide upcoming box when the delivery completed and no more shipment eg. pause plan
 
   return (
     <>
-      <p>
-        {t.rich('your-upcoming-box-is-arriving-on-the-{}', {
-          date: formatDate(t, shipable.deliveryDate, true),
-        })}{' '}
-        {t('it-contains-{}-fresh-food', {
-          value: new Intl.ListFormat('en-HK').format(
-            shipable.boxs.map((box) => t('{}-apostrophe', { value: box.dog.name }))
-          ),
-        })}
-      </p>
+      {shipable && (
+        <p>
+          {t.rich('your-upcoming-box-is-arriving-on-the-{}', {
+            date: formatDate(t, shipable.deliveryDate, true),
+          })}{' '}
+          {t('it-contains-{}-fresh-food', {
+            value: new Intl.ListFormat('en-HK').format(
+              shipable.boxs.map((box) => t('{}-apostrophe', { value: box.dog.name }))
+            ),
+          })}
+        </p>
+      )}
       {shipments[0].editableDeadline >= refDate && (
         <p className="mt-4">
           {t.rich('unfortunately-you-can-no-longer-make-changes-to-your-upcoming-box', {
