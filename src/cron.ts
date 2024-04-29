0f33acd54efba7d406be23ac4c0d1492d7006526
Ryan Har <ryan.har@amus.dev>
2024-04-29 20:04:29 +0800

    Update cron job

--- src/cron.ts
+++ src/cron.ts
@@ -1,11 +1,12 @@
 import { CronJob } from 'cron';
+
 import subscriptionScheduler from './schedulers/subscription';
 
 function cron() {
   new CronJob(
     '1 0 0 * * *', // run every day at 00:01
     subscriptionScheduler, // onTick
-    () => console.log(`compeleted subscription scheduler at ${new Date().toISOString()}`),
+    undefined,
     true, // start
     process.env.TZ, // timeZone
     undefined,
