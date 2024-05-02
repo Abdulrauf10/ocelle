36e04283b775f2f155f5e35ff6a058151ac48959
Ryan Har <ryan.har@amus.dev>
2024-05-03 02:19:43 +0800

    Update dog seeder

--- src/seeders/DogSeeder.ts
+++ src/seeders/DogSeeder.ts
@@ -1,27 +1,41 @@
+import { addDays, startOfDay } from 'date-fns';
 import { QueryRunner } from 'typeorm';
 
 import Seeder from './Seeder';
 
-import { Dog, DogPlan, User } from '@/entities';
+import { Dog, DogPlan, Order, RecurringBox, Shipment, User } from '@/entities';
 import { FoodAllergies, MealPlan, OrderSize, Recipe } from '@/enums';
+import { getClosestOrderDeliveryDate, getEditableRecurringBoxDeadline } from '@/helpers/dog';
+import { getCalendarEvents } from '@/services/calendar';
 
 export default class DogSeeder extends Seeder {
   /**
    * Clean the Collection
    */
   async clean(queryRunner: QueryRunner): Promise<void> {
+    const orderRepository = queryRunner.manager.getRepository(Order);
+    const shipmentRepository = queryRunner.manager.getRepository(Shipment);
+    const recurringBoxRepository = queryRunner.manager.getRepository(RecurringBox);
     const dogRepository = queryRunner.manager.getRepository(Dog);
     const dogPlanRepository = queryRunner.manager.getRepository(DogPlan);
     const userRepository = queryRunner.manager.getRepository(User);
+    await recurringBoxRepository.remove(await recurringBoxRepository.find());
+    await shipmentRepository.remove(await shipmentRepository.find());
+    await orderRepository.remove(await orderRepository.find());
     await dogPlanRepository.remove(await dogPlanRepository.find());
     await dogRepository.remove(await dogRepository.find());
     await userRepository.remove(await userRepository.find());
   }
 
   async run(queryRunner: QueryRunner): Promise<void> {
+    const events = await getCalendarEvents();
+    const today = startOfDay(new Date());
     const userRepository = queryRunner.manager.getRepository(User);
     const dogRepository = queryRunner.manager.getRepository(Dog);
     const dogPlanRepository = queryRunner.manager.getRepository(DogPlan);
+    const recurringBoxRepository = queryRunner.manager.getRepository(RecurringBox);
+    const shipmentRepository = queryRunner.manager.getRepository(Shipment);
+    const orderRepository = queryRunner.manager.getRepository(Order);
 
     const user = userRepository.create({
       orderSize: OrderSize.TwoWeek,
@@ -31,6 +45,23 @@ export default class DogSeeder extends Seeder {
     });
 
     await userRepository.save(user);
+
+    const deliveryDate = getClosestOrderDeliveryDate(events);
+
+    const order = orderRepository.create({
+      id: 'fake-order',
+      createdAt: new Date(),
+    });
+
+    await orderRepository.save(order);
+
+    const shipment = shipmentRepository.create({
+      deliveryDate,
+      editableDeadline: getEditableRecurringBoxDeadline(events, deliveryDate, true),
+      user,
+    });
+
+    await shipmentRepository.save(shipment);
 
     const dog1 = dogRepository.create({
       name: 'Charlie',
@@ -72,7 +103,7 @@ export default class DogSeeder extends Seeder {
       recipe2: Recipe.Pork,
       isEnabledTransitionPeriod: true,
       isEnabled: true,
-      startDate: new Date(),
+      startDate: today,
       dog: dog1,
     });
 
@@ -81,10 +112,38 @@ export default class DogSeeder extends Seeder {
       recipe1: Recipe.Chicken,
       isEnabledTransitionPeriod: true,
       isEnabled: true,
-      startDate: new Date(),
+      startDate: today,
       dog: dog2,
     });
 
     await dogPlanRepository.save([plan1, plan2]);
+
+    const box1 = recurringBoxRepository.create({
+      mealPlan: plan1.mealPlan,
+      orderSize: user.orderSize,
+      recipe1: plan1.recipe1,
+      recipe2: plan1.recipe2,
+      isTransitionPeriod: true,
+      startDate: today,
+      endDate: addDays(today, 14),
+      dog: dog1,
+      order,
+      shipment,
+    });
+
+    const box2 = recurringBoxRepository.create({
+      mealPlan: plan2.mealPlan,
+      orderSize: user.orderSize,
+      recipe1: plan2.recipe1,
+      recipe2: plan2.recipe2,
+      isTransitionPeriod: true,
+      startDate: today,
+      endDate: addDays(today, 14),
+      dog: dog2,
+      order,
+      shipment,
+    });
+
+    await recurringBoxRepository.save([box1, box2]);
   }
 }
