21eedcafea376d14207de232ef0d7677da9e6ff5
Ryan Har <ryan.har@amus.dev>
2024-05-05 22:15:30 +0800

    Add sup and sub default translation values

--- src/defaultTranslationValues.tsx
+++ src/defaultTranslationValues.tsx
@@ -4,6 +4,8 @@ const defaultTranslationValues: RichTranslationValues = {
   i: (chunks) => <i>{chunks}</i>,
   b: (chunks) => <b>{chunks}</b>,
   br: () => <br />,
+  sup: (chunks) => <sup>{chunks}</sup>,
+  sub: (chunks) => <sub>{chunks}</sub>,
   nowrap: (chunks) => <span className="whitespace-nowrap">{chunks}</span>,
   block: (chunks) => <span className="inline-block">{chunks}</span>,
 };
