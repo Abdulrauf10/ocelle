0476a41918995d465b4674e84f8c4986bc6f4356
Ryan Har <ryan.har@amus.dev>
2024-05-05 22:15:02 +0800

    Append common box shadow tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,9 @@ const config: Config = {
         'how-it-works-green': '#93D06D',
         'how-it-works-dark-green': '#269D9E',
       },
+      boxShadow: {
+        block: '5px 5px 20px rgba(0, 0, 0, 0.2)',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
