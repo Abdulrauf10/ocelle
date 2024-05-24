2f070c9b068648c9731c277c2ebaa422409dcd00
11jackel <jackel.shiu@amus.dev>
2024-05-24 17:48:20 +0800

    Ocelle_Change Log_FAQ (P007)_v0.14

--- src/app/[locale]/(main)/faq/page.tsx
+++ src/app/[locale]/(main)/faq/page.tsx
@@ -35,7 +35,7 @@ export default function FaQ() {
   return (
     <main>
       <div
-        className="bg-[#4b8dbd] bg-[url('/faq_banner_img.jpg')] bg-[length:auto_100%] bg-[44%_center] bg-repeat-x py-[12em] max-2xl:py-[11%] 
+        className="bg-[#4b8dbd] bg-[url('/faq_banner_img.jpg')] bg-[length:auto_100%] bg-[44%_center] bg-no-repeat py-[12em] max-2xl:py-[11%] 
                   max-md:min-h-[800px] max-md:bg-[url('/ocelle-faq-mb-bg.jpg')] max-md:bg-[length:768px_960px] max-md:bg-[bottom_center]  max-md:bg-no-repeat max-md:pb-8
                   max-md:pt-4"
       >
@@ -44,7 +44,7 @@ export default function FaQ() {
             <h1 className="-ml-[9px] select-none text-[215px] font-bold leading-none text-white drop-shadow-[5px_5px_12px_rgba(0,0,0,.5)] max-lg:text-[130px]">
               FAQ
             </h1>
-            <p className="body-2 text-white">
+            <p className="body-1 text-white">
               If you can’t find what you’re looking for, please contact us!
             </p>
             <div className="mt-6">
@@ -101,7 +101,7 @@ export default function FaQ() {
                 <span className="heading-4 font-bold text-white">Subscription</span>
               </Link>
             </div>
-            <div className="flex justify-center p-2 md:flex-[1_1_40%] xl:flex-1">
+            <div className="flex p-2 md:flex-[1_1_40%] md:justify-end md:pr-[5%] xl:flex-1 xl:justify-center xl:pr-2">
               <Link
                 href="#deliveries"
                 className="block w-[220px] whitespace-nowrap rounded-3xl bg-primary px-10 py-1 text-center hover:opacity-90"
@@ -109,7 +109,7 @@ export default function FaQ() {
                 <span className="heading-4 font-bold text-white">Deliveries</span>
               </Link>
             </div>
-            <div className="flex justify-center p-2 md:flex-[1_1_40%] xl:flex-1">
+            <div className="flex p-2 md:flex-[1_1_40%] md:justify-start md:pl-[5%] xl:flex-1 xl:justify-center xl:pl-2">
               <Link
                 href="#payment"
                 className="block w-[220px] whitespace-nowrap rounded-3xl bg-primary px-10 py-1 text-center hover:opacity-90"
@@ -121,7 +121,7 @@ export default function FaQ() {
         </Container>
       </div>
       <div className="bg-brown bg-opacity-10 py-16">
-        <Container className="-my-8 max-w-screen-xl">
+        <Container className="-my-8 max-w-8xl">
           <Block
             id="our-food"
             title="OUR FOOD"
