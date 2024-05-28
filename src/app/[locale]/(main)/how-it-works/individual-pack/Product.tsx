ae9dd357c6c95f4df0a88fa8405dcae6e99d606f
Ryan Har <ryan.har@amus.dev>
2024-05-28 18:39:31 +0800

    Update individual packs add to cart workflow

--- src/app/[locale]/(main)/how-it-works/individual-pack/Product.tsx
+++ src/app/[locale]/(main)/how-it-works/individual-pack/Product.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 import React from 'react';
 import xss from 'xss';
 
-import { addToCart } from './actions';
+import { addToCart, updateCartLine } from './actions';
 
 import Container from '@/components/Container';
 import Button from '@/components/buttons/Button';
@@ -18,6 +18,7 @@ import { useCart } from '@/contexts/cart';
 import { IndividualRecipePack } from '@/enums';
 import { ProductFragment } from '@/gql/graphql';
 import { weightToGrams } from '@/helpers/saleor';
+import { individualPackProducts } from '@/products';
 
 const parser = edjsHTML();
 
@@ -55,14 +56,28 @@ export default function Product({
 }) {
   const t = useTranslations();
   const b = useTranslations('Button');
-  const { setLines, setTotalPrice } = useCart();
-  const [quantity, setQuantity] = React.useState(1);
+  const { setLines, setTotalPrice, lines } = useCart();
+  const currentLine = React.useMemo(() => {
+    return lines.find((line) => line.variant.product.slug === individualPackProducts[pack].slug);
+  }, [lines, pack]);
+
+  const handleQuantityChange = React.useCallback(
+    async (quantity: number) => {
+      if (!currentLine) {
+        return;
+      }
+      const { lines, totalPrice } = await updateCartLine(currentLine.id, quantity);
+      setLines(lines);
+      setTotalPrice(totalPrice);
+    },
+    [currentLine, setLines, setTotalPrice]
+  );
 
   const handleButtonClick = React.useCallback(async () => {
-    const { lines, totalPrice } = await addToCart(pack, quantity);
+    const { lines, totalPrice } = await addToCart(pack, 1);
     setLines(lines);
     setTotalPrice(totalPrice);
-  }, [pack, quantity, setLines, setTotalPrice]);
+  }, [pack, setLines, setTotalPrice]);
 
   const getPrice = (product: ProductFragment) => {
     return product.variants![0].channelListings![0].price!.amount;
@@ -124,26 +139,29 @@ export default function Product({
                 </RecipeMediumDialog>
               </div>
             )}
-            <label className="mt-6 block">
-              <span className={clsx('body-1 body-weight-1 mr-3 inline-block', className.content)}>
-                {t('{}-colon', { value: t('quantity') })}
-              </span>
-              <NumberInput
-                className={{
-                  root: 'w-20 border-brown',
-                  input: 'body-1 body-inline w-[78px] px-4 py-1',
-                  icon: 'w-2.5',
-                }}
-                min={1}
-                value={quantity}
-                onChange={setQuantity}
-              />
-            </label>
-            <div className="mt-6">
-              <Button type="button" theme={theme} onClick={handleButtonClick}>
-                {t('add-to-cart')}
-              </Button>
-            </div>
+            {currentLine ? (
+              <label className="mt-6 block">
+                <span className={clsx('body-1 body-weight-1 mr-3 inline-block', className.content)}>
+                  {t('{}-colon', { value: t('quantity') })}
+                </span>
+                <NumberInput
+                  className={{
+                    root: 'w-20 border-brown',
+                    input: 'body-1 body-inline w-[78px] px-4 py-1',
+                    icon: 'w-2.5',
+                  }}
+                  min={0}
+                  value={currentLine.quantity}
+                  onChange={handleQuantityChange}
+                />
+              </label>
+            ) : (
+              <div className="mt-6">
+                <Button type="button" theme={theme} onClick={handleButtonClick}>
+                  {t('add-to-cart')}
+                </Button>
+              </div>
+            )}
           </div>
         </div>
       </Container>
