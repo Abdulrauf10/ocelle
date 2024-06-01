c1a1cd8c75c6a19bf499816b359c899e70a01c4c
Ryan Har <ryan.har@amus.dev>
2024-06-01 11:23:42 +0800

    Update 404

--- src/app/[locale]/[...catch]/page.tsx
+++ src/app/[locale]/[...catch]/page.tsx
@@ -20,36 +20,30 @@ export default function NotFound() {
       <Promotion />
       <Header nav={<Navigator />} />
       <div className="bg-gold bg-opacity-20">
-        <Container className="flex max-w-screen-xl items-center max-[903px]:flex-col max-[903px]:items-center">
-          <div className="w-full py-20 text-left max-[903px]:py-8 max-[903px]:text-center">
+        <Container className="flex max-w-screen-xl items-center max-[951px]:flex-col max-[951px]:items-center">
+          <div className="w-full py-20 max-[951px]:py-8 max-[951px]:text-center">
             <h1 className="heading-1 font-bold text-primary">
               <span className="inline-block">
                 <i>Aiya! </i>
               </span>{' '}
               <span className="inline-block">Page Gone Walkies!</span>
             </h1>
-            <div className="mt-4">
-              <p className="body-1">
-                <span className="text-primary">
-                  Please use the links below. <i>Woof!</i>
-                </span>
-              </p>
-            </div>
-
-            <div className="mx-0 max-w-[180px] max-[903px]:mx-auto">
-              <div className="mt-4">
-                <Button fullWidth onClick={() => router.back()}>
-                  <span className="flex-1">{t('go-back')}</span>
-                </Button>
-              </div>
-              <div className="mt-4">
-                <Button fullWidth href="/">
-                  <span className="flex-1">{t('go-home')}</span>
-                </Button>
-              </div>
+            <div className="pt-4"></div>
+            <p className="body-1 text-primary">
+              Please use the links below. <i>Woof!</i>
+            </p>
+            <div className="pt-4"></div>
+            <div className="mx-0 max-w-[180px] max-[951px]:mx-auto">
+              <Button fullWidth onClick={() => router.back()}>
+                <span className="flex-1">{t('go-back')}</span>
+              </Button>
+              <div className="pt-4"></div>
+              <Button fullWidth href="/">
+                <span className="flex-1">{t('go-home')}</span>
+              </Button>
             </div>
           </div>
-          <div className="w-full px-4 pt-0 xs:w-[480px] xs:min-w-[530px] min-[903px]:pt-24">
+          <div className="w-full max-w-full pt-24 max-[951px]:pt-0 xs:w-[500px] min-[952px]:min-w-[530px] min-[952px]:px-4">
             <div className="relative pt-[100%]">
               <Image src="/404.png" alt="404 dog" fill />
             </div>
