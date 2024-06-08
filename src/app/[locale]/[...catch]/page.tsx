ae3bd4a3e1ef040fbc0b2b0dbce3de33c8fa2a51
Ryan Har <ryan.har@amus.dev>
2024-06-08 21:36:39 +0800

    Fix Ocelle_Change Log_404 Error (P015)_v0.20 - R6

--- src/app/[locale]/[...catch]/page.tsx
+++ src/app/[locale]/[...catch]/page.tsx
@@ -21,13 +21,13 @@ export default function NotFound() {
       <Promotion />
       <Header nav={<Navigator />} />
       <div className="bg-gold bg-opacity-20">
-        <Container className="flex max-w-screen-xl items-center max-[951px]:flex-col max-[951px]:items-center">
-          <div className="w-full py-20 max-[951px]:py-8 max-[951px]:text-center">
-            <h1 className="heading-1 font-bold text-primary">{n.rich('block-1-title')}</h1>
+        <Container className="flex items-center max-[1070px]:flex-col max-[1070px]:items-center">
+          <div className="w-full py-20 max-[1070px]:py-8 max-[1070px]:text-center">
+            <h1 className="heading-headline font-bold text-primary">{n.rich('block-1-title')}</h1>
             <div className="pt-4"></div>
             <p className="body-1 text-primary">{n.rich('block-1-content')}</p>
             <div className="pt-4"></div>
-            <div className="mx-0 max-w-[180px] max-[951px]:mx-auto">
+            <div className="mx-0 max-w-[180px] max-[1070px]:mx-auto">
               <Button fullWidth onClick={() => router.back()}>
                 <span className="flex-1">{t('go-back')}</span>
               </Button>
@@ -37,7 +37,7 @@ export default function NotFound() {
               </Button>
             </div>
           </div>
-          <div className="w-full max-w-full pt-24 max-[951px]:pt-0 xs:w-[500px] min-[952px]:min-w-[530px] min-[952px]:px-4">
+          <div className="w-full max-w-full pt-24 max-[1070px]:pt-0 xs:w-[500px] min-[952px]:min-w-[530px] min-[952px]:px-4">
             <div className="relative pt-[100%]">
               <Image src="/404.png" alt="404 dog" fill />
             </div>
