dfe1806f8135facc808689e56301a4a3c1d3e92b
Ryan Har <ryan.har@amus.dev>
2024-06-09 12:15:28 +0800

    Fix recipe checkbox drop shadow bug

--- src/components/controls/RecipeCheckbox.tsx
+++ src/components/controls/RecipeCheckbox.tsx
@@ -50,14 +50,47 @@ export default function RecipeCheckbox<T extends FieldValues>({
   const t = useTranslations();
   const b = useTranslations('Button');
   return (
-    <div
-      className={clsx(
-        'drop-shadow-style-1 relative mx-auto mt-[70px] min-w-[230px] max-w-[280px] rounded-[20px] border p-[10px] shadow-black/20',
-        disabled
-          ? 'pointer-events-none select-none border-[#7B8D97] bg-[#F2F4F5]'
-          : 'border-gold bg-white'
-      )}
-    >
+    <div className="relative mx-auto mt-[70px] min-w-[230px] max-w-[280px]">
+      <div
+        className={clsx(
+          'drop-shadow-style-1 rounded-[20px] border p-[10px] shadow-black/20',
+          disabled
+            ? 'pointer-events-none select-none border-[#7B8D97] bg-[#F2F4F5]'
+            : 'border-gold bg-white'
+        )}
+      >
+        <div className="h-[70px]"></div>
+        <div className={clsx('mt-2 text-center', disabled ? 'text-[#BDC6CB]' : 'text-gold')}>
+          <RoundedCheckbox
+            name={name}
+            control={control}
+            rules={rules}
+            error={error}
+            label={title}
+            className="font-bold text-gold"
+            disabled={disabled}
+            onChange={onChange}
+          />
+          <div className={clsx('mt-0.5 text-[#7B8D97]', disabled && 'text-opacity-50')}>
+            {price === 'cheap' ? '$$' : price === 'normal' ? '$$$' : '$$$$'}
+          </div>
+          <div className="mt-0.5">
+            <RecipeMediumDialog
+              name={title}
+              description={description}
+              picture={dialogPicture}
+              ingredients={ingredients}
+              targetedNutrientBlendIngredients={targetedNutrientBlendIngredients}
+              calorie={calorie}
+              analysis={analysis}
+            >
+              <button type="button" className="font-light underline">
+                {b('see-details')}
+              </button>
+            </RecipeMediumDialog>
+          </div>
+        </div>
+      </div>
       <div className="absolute -top-[70px] left-1/2 -translate-x-1/2">
         {recommended && (
           <div
@@ -75,37 +108,6 @@ export default function RecipeCheckbox<T extends FieldValues>({
           height={140}
           className="drop-shadow-style-5 min-w-[140px] rounded-2xl shadow-black/20"
         />
-      </div>
-      <div className="h-[70px]"></div>
-      <div className={clsx('mt-2 text-center', disabled ? 'text-[#BDC6CB]' : 'text-gold')}>
-        <RoundedCheckbox
-          name={name}
-          control={control}
-          rules={rules}
-          error={error}
-          label={title}
-          className="font-bold text-gold"
-          disabled={disabled}
-          onChange={onChange}
-        />
-        <div className={clsx('mt-0.5 text-[#7B8D97]', disabled && 'text-opacity-50')}>
-          {price === 'cheap' ? '$$' : price === 'normal' ? '$$$' : '$$$$'}
-        </div>
-        <div className="mt-0.5">
-          <RecipeMediumDialog
-            name={title}
-            description={description}
-            picture={dialogPicture}
-            ingredients={ingredients}
-            targetedNutrientBlendIngredients={targetedNutrientBlendIngredients}
-            calorie={calorie}
-            analysis={analysis}
-          >
-            <button type="button" className="font-light underline">
-              {b('see-details')}
-            </button>
-          </RecipeMediumDialog>
-        </div>
       </div>
     </div>
   );
