5e8e6a73bbfbccf04fd1a7b9171f8b6143930ae3
11jackel <jackel.shiu@amus.dev>
2024-08-11 23:26:39 +0800

    Why Fresh - Kibble - TC - * - V.2 (0806)

--- src/defaultTranslationValues.tsx
+++ src/defaultTranslationValues.tsx
@@ -19,6 +19,7 @@ const defaultTranslationValues: RichTranslationValues = {
   sub: (chunks) => <sub className="body-4 inline-block">{chunks}</sub>,
   nowrap: (chunks) => <span className="whitespace-nowrap">{chunks}</span>,
   block: (chunks) => <span className="inline-block">{chunks}</span>,
+  mbblock: (chunks) => <span className="inline max-sm:inline-block">{chunks}</span>,
   baskerville: (chunks) => (
     <span className="relative -top-px font-baskerville font-bold">{chunks}</span>
   ),
